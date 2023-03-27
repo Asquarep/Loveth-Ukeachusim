9b57315a8f116e91d739123b47fb686d714a0358
Asquarep <asquarep@yahoo.com>
2023-03-27 14:39:04 +0100

    stock-api integrated

---
+++ stock-trading-app/src/routes/route.js
@@ -0,0 +1,25 @@
+import Dashboard from "../pages/DashBoard";
+import LoginPage from "../pages/LoginPage";
+import SignUpPage from "../pages/SignUpPage";
+
+export const publicRoutes = [
+    
+    {
+      title: "Login",
+      path: "login",
+      element: LoginPage,
+    },
+    {
+        title: "Sign Up",
+        path: "signup",
+        element: SignUpPage,
+      }
+]
+
+export const privateRoutes = [
+    {
+        title: "Dashboard",
+        path: "dashboard",
+        element: Dashboard,
+      }
+]
